987b8cc60af09d9983ed2436671ccc5be26f4329
zoeywang <zwang297@gmail.com>
2016-11-30 23:55:49 -0500

    Add files via upload

---
+++ atoi_8.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+public class atoi_8 {
+
+	public static void main(String[] args){
+		System.out.println(myAtoi("2147483648"));
+		//System.out.println(Integer.MAX_VALUE);
+	}
+	
+	public static int myAtoi(String str) {
+		int result = 0;
+        int j=0;
+        int mark = 1;
+        int sign = 0;
+        int value;
+        if(str.length()==0)
+        	return 0;
+        while(str.charAt(j)==' '){
+        	j++;
+        }
+        for(int i=j;i<str.length();i++){
+        	if(str.charAt(i)=='-'||str.charAt(i)=='+')
+        		sign++;
+        	System.out.println(sign);
+        	if(sign>1)
+        		return 0;
+        	if(str.charAt(i)=='-')
+        		mark = -1*mark;
+        	else if(str.charAt(i)=='+')
+        	{System.out.println("mark");
+        	    mark = 1*mark;
+        	}
+        	else if(str.charAt(i)<='9'&&str.charAt(i)>='0'){
+        		value = str.charAt(i)-'0';
+        		System.out.println(value);
+        		//System.out.println(result*10+value);
+        		System.out.println("0-9");
+        		if(result*10+7==Integer.MAX_VALUE&&value>7){
+        			System.out.println("max");
+        			return Integer.MAX_VALUE;}
+        		if(mark*result*10+8==Integer.MIN_VALUE&&value>8)
+        			return Integer.MIN_VALUE;
+        		//System.out.println(str.charAt(i));
+        		
+        		result = result*10 + value;
+        		System.out.println(result);
+        	}else{
+        	    return mark*result;
+        	}
+        }
+        return mark*result;
+    }
+	
+}
