6e53f96e2936bc08f59c5d0e61c01f2672986439
zoeywang <zwang297@gmail.com>
2017-01-31 15:29:06 -0500

    Add files via upload

--- SearchInsertPosition_35.java
+++ SearchInsertPosition_35.java
@@ -5,16 +5,84 @@ public class SearchInsertPosition_35 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int a[] = {1,2,2,2,3};
+		System.out.println(searchInsert3(a, 2));
+		
 	}
 	
-	public int searchInsert(int[] nums, int target) {
+	public static int searchInsert(int[] nums, int target) {
+		int l = nums.length;
+        int r = search(nums,target,0,l-1);
+        System.out.println(r+"ha");
+        return r>-1?r:l;
+    }
+	
+	public static int search(int nums[],int target, int i, int j){
+		int index = i+(j-i)/2;
+		int result=-1;
+		if(index==j){
+			System.out.println("roof");
+			return result;}
+		if(nums[index]<target){
+				search(nums,target,index+1,j);
+		}else{
+			result = index;
+			System.out.println(result);
+			if(index==0){
+				System.out.println("first result "+result);
+				return result;
+			}else
+				search(nums,target,i,index-1);
+		}return result;
+	}
+	
+	public static int searchInsert2(int[] nums, int target) {
         int l = nums.length;
-        for(int i=0;i<l;i++){
-            if(target<=nums[i]){
-                return i;
+        int i = 0;
+        int j = l-1;
+        while(i<j){
+            int index = i + (j-i)/2;
+            if(nums[index]==target){
+                 return index;
             }
+            else if(nums[index]<target)
+                i = index+1;
+            else
+                j = index-1;
         }
-        return l;
+        if(nums[i]>=target)
+        {
+            return i;
+        }else
+            return i+1;
+    }
+	
+	public static int searchInsert3(int[] nums, int target) {
+        int l = nums.length;
+        int i = 0;
+        int j = l-1;
+        while(i<j){
+            int index = i + (j-i)/2;
+            if(nums[index]==target){
+                 int result = index;
+                 while(index>-1){
+                     if(nums[index]==target)
+                        result = index;
+                     else break;
+                     index--;
+                 }
+                 return result;
+            }
+            else if(nums[index]<target)
+                i = index+1;
+            else
+                j = index-1;
+        }
+        if(nums[i]>=target)
+        {
+            return i;
+        }else
+            return i+1;
     }
 
 }
