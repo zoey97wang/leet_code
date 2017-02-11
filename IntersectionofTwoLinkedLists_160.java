58c1c9f9ae804d984ad34090114cc467cedc6816
zoeywang <zwang297@gmail.com>
2017-02-11 11:47:31 -0500

    Add files via upload

--- IntersectionofTwoLinkedLists_160.java
+++ IntersectionofTwoLinkedLists_160.java
@@ -31,5 +31,48 @@ public class IntersectionofTwoLinkedLists_160 {
         }
         return null;
     }
+	
+	public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        ListNode la = headA;
+        ListNode lb = headB;
+        while(la!=lb){
+            if(la==null)
+                la = headB;
+            else
+                la = la.next;
+            if(lb==null)
+                lb = headA;
+            else
+                lb = lb.next;
+        }
+        return la;
+    }
+	
+	public int length(ListNode head){
+		int result=0;
+		while(head!=null){
+			result++;
+			head = head.next;
+		}
+		return result;
+	}
+	
+	public ListNode getIntersectionNode3(ListNode headA, ListNode headB){
+		int lengthA = length(headA);
+		int lengthB = length(headB);
+		while(lengthA>lengthB){
+			headA=headA.next;
+			lengthA--;
+		}
+		while(lengthB>lengthA){
+			headB=headB.next;
+			lengthB--;
+		}
+		while(headA!=headB){
+			headA = headA.next;
+			headB = headB.next;
+		}
+		return headA;
+	}
 
 }
