f9a23187340c1873d5996b745083814289229711
zoeywang <zwang297@gmail.com>
2017-03-24 22:15:42 -0400

    Add files via upload

--- PalindromePartitioning_131.java
+++ PalindromePartitioning_131.java
@@ -16,20 +16,26 @@ public class PalindromePartitioning_131 {
 	}
 	
 	public List<List<String>> partition(String s) {
+        System.out.println("sTRING"+s);
         List<List<String>> result = new LinkedList<List<String>>();
         int l = s.length();
         for(int i=0;i<l;i++){
             if(helper(s,0,i)){
-                System.out.println(i);
-                result = partition(s.substring(i+1,l));
-                if(result.size() == 0){
+                System.out.println(s.charAt(i));
+                List<List<String>> temp = new LinkedList<List<String>>();
+                if(i!=l-1){
+                temp = partition(s.substring(i+1,l));
+                }
+                if(temp.size() == 0){
                     LinkedList<String> ll = new LinkedList<String>();
                     ll.add(s.substring(0,i+1));
-                    result.add(ll);
+                    temp.add(ll);
                 }else{
-                    for(List temp:result){
-                        temp.add(s.substring(0,i+1));
+                    for(List tp:temp){
+                        tp.add(s.substring(0,i+1));
                     }
+                }for(List t:temp){
+                    result.add(t);
                 }
             }
         }
