eeea16707a02301f42bb6748d4f8dee54567f58a
zoeywang <zwang297@gmail.com>
2017-03-30 12:12:18 -0400

    Add files via upload

---
+++ MinimumAbsoluteDifferenceinBST_530.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MinimumAbsoluteDifferenceinBST_530 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	 public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	 }
+
+	    int min = Integer.MAX_VALUE;
+	    List<Integer> l = new LinkedList<>();
+	    public int getMinimumDifference(TreeNode root) {
+	        helper(root);
+	        int len = l.size();
+	        int[] a = new int[len];
+	        for(int i=0;i<len;i++){
+	            a[i] = l.get(i);
+	        }
+	        for(int j=0;j<len;j++){
+	            for(int x=j+1;x<len;x++){
+	                int d = a[j]-a[x];
+	                min = Math.min(min,Math.abs(d));
+	            }
+	        }
+	        return min;
+	    }
+	    public void helper(TreeNode root) {
+	        if(root == null)
+	            return;
+	        l.add(root.val);
+	        helper(root.left);
+	        helper(root.right);
+	    }
+
+}
