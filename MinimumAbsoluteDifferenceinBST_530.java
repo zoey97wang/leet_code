08be9c14920d87c0450a7f1be394663f8d0d98ef
zoeywang <zwang297@gmail.com>
2017-03-30 16:33:39 -0400

    Add files via upload

--- MinimumAbsoluteDifferenceinBST_530.java
+++ MinimumAbsoluteDifferenceinBST_530.java
@@ -5,6 +5,22 @@ import java.util.List;
 
 public class MinimumAbsoluteDifferenceinBST_530 {
 
+	public class Solution {
+	    int min = Integer.MAX_VALUE;
+	    Integer pre = null;
+	    public int getMinimumDifference(TreeNode root) {
+	        if(root == null)
+	            return min;
+	        getMinimumDifference(root.left);
+	        if(pre!=null){
+	            min = Math.min(min, root.val - pre);
+	        }
+	        pre = root.val;
+	        getMinimumDifference(root.right);
+	        return min;
+	    }
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
