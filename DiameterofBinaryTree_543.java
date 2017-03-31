6950d3ef105b5d25ccb9ac17227b0251139a43b3
zoeywang <zwang297@gmail.com>
2017-03-31 15:17:26 -0400

    Add files via upload

--- DiameterofBinaryTree_543.java
+++ DiameterofBinaryTree_543.java
@@ -36,5 +36,21 @@ public class DiameterofBinaryTree_543 {
 	        return Math.max(getMax(root.left,len),getMax(root.right,len));
 	    }
 	}
+	
+	public class Solution2 {
+	    Integer max = 0;
+	    public int diameterOfBinaryTree(TreeNode root) {
+	        getMax(root);
+	        return max;
+	    }
+	    public int getMax(TreeNode root){
+	        if(root == null)
+	            return 0;
+	        int left = getMax(root.left);
+		    int right = getMax(root.right);
+	        max = Math.max(max, left+right);
+	        return Math.max(left, right)+1;
+	    }
+	}
 
 }
