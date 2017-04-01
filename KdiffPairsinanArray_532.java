057e5a5f68a6ca748f49d982fafb80daeb0ba389
zoeywang <zwang297@gmail.com>
2017-03-31 23:31:50 -0400

    Add files via upload

---
+++ KdiffPairsinanArray_532.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.HashSet;
+
+public class KdiffPairsinanArray_532 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public class Solution {
+	    public int findPairs(int[] nums, int k) {
+	        if(k<0)
+	            return 0;
+	        HashSet<Integer> h = new HashSet<>();
+	        int l = nums.length;
+	        for(int i =0;i<l;i++){
+	            for(int j=i+1;j<l;j++){
+	                if(nums[i]-nums[j]==k||nums[i]-nums[j]==-k)
+	                    h.add(nums[i]+ nums[j]);
+	            }
+	        }
+	        return h.size();
+	    }
+	}
+
+}
