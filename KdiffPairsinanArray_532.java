b6fca22d259c4d7e3f3d8782ec55077cb760197a
zoeywang <zwang297@gmail.com>
2017-03-31 23:36:46 -0400

    faster version

--- KdiffPairsinanArray_532.java
+++ KdiffPairsinanArray_532.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class KdiffPairsinanArray_532 {
@@ -19,6 +20,23 @@ public class KdiffPairsinanArray_532 {
 	            for(int j=i+1;j<l;j++){
 	                if(nums[i]-nums[j]==k||nums[i]-nums[j]==-k)
 	                    h.add(nums[i]+ nums[j]);
+	            }
+	        }
+	        return h.size();
+	    }
+	    
+	    public int findPairs2(int[] nums, int k) {
+	        if(k<0)
+	            return 0;
+	        HashSet<Integer> h = new HashSet<>();
+	        Arrays.sort(nums);
+	        int l = nums.length;
+	        for(int i =0;i<l;i++){
+	            for(int j=i+1;j<l;j++){
+	                if(nums[i]-nums[j]==k||nums[i]-nums[j]==-k)
+	                  { h.add(nums[i]+ nums[j]);
+	                      break;
+	                  }
 	            }
 	        }
 	        return h.size();
